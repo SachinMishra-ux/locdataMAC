0df5afba0d710a18c09374f431b670af389b71f4
Sachin mishra <63257356+SachinMishra-ux@users.noreply.github.com>
2023-07-12 21:01:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import io
 with io.open('README.md', encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.9"
+__version__ = "0.1.0"
 
 REPO_NAME = "locdataMAC"
 AUTHOR_USER_NAME = "SachinMishra-ux"
